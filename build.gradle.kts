928e68a0eb435c2ee6ffb83d69a965bac42a5ac5
oxcm07 <oxcm0727@gmail.com>
2026-07-18 21:38:37 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.4.0"
+}
+
+group = "com"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
